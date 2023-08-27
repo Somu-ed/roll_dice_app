ab36f0004e5c178e0c030f7a1912899f50891412
Soumyajeet Satpathy <jarvis@pop-os.localdomain>
2023-08-28 00:07:16 +0530

    adding custom widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 75, 1, 124),
+            Color.fromARGB(255, 99, 1, 165),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('Hello World!'),
-  ));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    ),
+  );
 }
